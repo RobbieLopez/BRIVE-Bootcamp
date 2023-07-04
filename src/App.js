dc3ca529bfefce4983c0530108bd1d461be8efb9
RobbieLopez <rvalderrama44@gmail.com>
2023-07-04 16:56:11 -0600

    Subida de codigo, primer version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import { Characters } from './components/Characters';
 function App() {
+  const [characters, setCharacters] = useState([]);
+  function getCharacters(pageNumber = 1) {
+    const res = fetch("https://rickandmortyapi.com/api/character/?page=1git s")
+      .then(response => response.json())
+      .then(({ results }) => results)
+      .catch(() => []);
+      return res;
+  }
+  async function consoleCharacters() {
+    const resp = await getCharacters();
+    setCharacters(resp);
+  }
+  //Comentrario agregado prueba git
+
+  //Es un hooked que nos permite ejecutar cierta funcionalidad en el ciclo de vida de cierto compoenente
+  useEffect(()=>{
+    console.log(consoleCharacters()) 
+  }, []);
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Header">
+        <img className="Logo" src="/logoRM.png" alt=""/>
+        <h1 className="Terms">Terms + conditions</h1>
       </header>
+      <div className="Hero">
+        <h1>Rick and morty</h1>
+        <h1>See all the caracters and more </h1>
+      </div>
+      <main>
+  <h1>Characters list</h1>
+  <hr />
+  {
+  (characters && characters.length > 0) && characters.map(character => (
+    <Characters character={character} /> 
+  ))
+  }
+</main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
