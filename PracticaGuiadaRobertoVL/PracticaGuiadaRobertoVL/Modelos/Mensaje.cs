b95c8be8babd878ae395a0c61cd0eb5e55be3db7
RobbieLopez <rvalderrama44@gmail.com>
2023-07-06 09:25:48 -0600

    primer tarea back

---
+++ PracticaGuiadaRobertoVL/PracticaGuiadaRobertoVL/Modelos/Mensaje.cs
@@ -0,0 +1,21 @@
+﻿namespace PracticaGuiadaRobertoVL.Modelos
+{
+    public class Mensaje
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }  
+        public Mensaje(string NombreIn, int EdadIn)
+        {
+            Nombre = NombreIn;
+            Edad = EdadIn;
+        }
+        //Practica propiedades en clase eprsona o heredada, clase padre
+        //y de la clase heredada 
+        //Objetivo de la practica, clase principal, una hijo y utilizar
+        //la que tiene la herencia, recibir N cantidad de paramtero,
+        //y en un mensaje devolver el contenido de la clase heredada
+        //En este caso personaPueso, no clase principal,sino la heredada
+        //Devolver todas las propiedades con herencioa
+
+    }
+}
---
+++ PracticaGuiadaRobertoVL/PracticaGuiadaRobertoVL/Modelos/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace PracticaGuiadaRobertoVL.Modelos
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Student : Person
+    {
+        public string StudentId { get; set; }
+        public string School { get; set; }
+    }
+    
+}
